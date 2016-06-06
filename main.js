1661e39ea4336830d1e7ca41c1ad623f44d35ec1
Alexey Ershov <alex4rom@gmail.com>
2016-06-06 18:21:42 +0300

    Add slider

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let appLinks = document.querySelectorAll('.control-button');
   let descriptionLinks = document.querySelectorAll('.description-item');
   let appLinkNum;
+
+  let intId = startSlider();
   
   document.addEventListener('click', function(e) {
     for (let i = 0; i < appLinks.length; i++) {
@@ -10,8 +12,24 @@ document.addEventListener('DOMContentLoaded', function(event) {
         appLinkNum = parseInt(e.target.className.match(/\d+/g)[0], 10);
         descriptionLinks[i].classList.remove('js-is-visible');
         descriptionLinks[appLinkNum - 1].classList.add('js-is-visible');
+
+        clearInterval(intId);
+        intId = startSlider();
       }
     }
   }, false);
+
+  function startSlider() {
+    let intervalId = setInterval(function() {
+      if (appLinkNum < appLinks.length) {
+        appLinks[appLinkNum].click();
+      } else {
+        appLinkNum = 0;
+        appLinks[appLinkNum].click();
+      }
+    }, 5000);
+
+    return intervalId;
+  }
 
 }, false);
